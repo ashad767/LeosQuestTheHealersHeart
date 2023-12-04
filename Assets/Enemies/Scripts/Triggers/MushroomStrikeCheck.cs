f0c2190fa2efedc09c4ac2cae6d3e15e37c561d3
ashad767 <ashadahmed247@gmail.com>
2023-12-04 00:16:31 -0500

    finished mm, added gg, mushroom, shadow imp

---
+++ Assets/Enemies/Scripts/Triggers/MushroomStrikeCheck.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomStrikeCheck : MonoBehaviour
+{
+    public Player player;
+    public Animator anim;
+    public int damage = 3;
+    private float timer = 0;
+    public bool dmg = false;
+    public AudioSource audioSource;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 3 && dmg)
+        {
+            timer += Time.deltaTime;
+            player.TakeDamage(Time.deltaTime * damage);
+        }
+        else if(timer > 3)
+            Destroy(gameObject);
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            dmg = true;
+            anim.SetBool("Explode", true);
+            audioSource.Play();
+        }
+    }
+}
